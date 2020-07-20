b3e6c3a1c6fd5cb764111cfbb7357792adb01522
Gerardo Gallardo <geris.g@gmail.com>
2020-07-20 14:54:03 -0300

    Add font simpsons (akbar). Fix images url.

--- site/app.js
+++ site/app.js
@@ -18,4 +18,5 @@ app.get("/login", (req,res) => (res.sendFile(__dirname +"/views/login.html")));
 app.get("/register", (req,res) => (res.sendFile(__dirname +"/views/register.html")));
 
 // Temporal
+app.get("/header", (req,res) => (res.sendFile(__dirname +"/views/partials/header.html")));
 app.get("/footer", (req,res) => (res.sendFile(__dirname +"/views/partials/footer.html")));
