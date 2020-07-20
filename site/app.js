ffbf9ce99a09d864565c4d6718e5d9d51e81b8b0
Gerardo Gallardo <geris.g@gmail.com>
2020-07-20 14:23:21 -0300

    Initial footer (mobile first)

--- site/app.js
+++ site/app.js
@@ -7,12 +7,15 @@ app.use (express.urlencoded({extended:false}));
 app.listen (3000, ()=> (console.log("Servidor funcionando en puerto 3000")));
 
 
-app.get ("/", (req,res) => (res.sendFile(__dirname +"/views/index.html")));
+app.get("/", (req,res) => (res.sendFile(__dirname +"/views/index.html")));
 
-app.get ("/cart", (req,res) => (res.sendFile(__dirname +"/views/cart.html")));
+app.get("/cart", (req,res) => (res.sendFile(__dirname +"/views/cart.html")));
 
-app.get ("/product", (req,res) => (res.sendFile(__dirname +"/views/product.html")));
+app.get("/product", (req,res) => (res.sendFile(__dirname +"/views/product.html")));
 
-app.get ("/login", (req,res) => (res.sendFile(__dirname +"/views/login.html")));
+app.get("/login", (req,res) => (res.sendFile(__dirname +"/views/login.html")));
 
-app.get ("/register", (req,res) => (res.sendFile(__dirname +"/views/register.html")));
+app.get("/register", (req,res) => (res.sendFile(__dirname +"/views/register.html")));
+
+// Temporal
+app.get("/footer", (req,res) => (res.sendFile(__dirname +"/views/partials/footer.html")));
