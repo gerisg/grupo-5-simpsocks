4f563fa6f08c3c7c0e5c569bf34d7f725065e91c
Gerardo Gallardo <geris.g@gmail.com>
2020-07-27 02:04:07 -0300

    Added links to Header and Home

--- site/app.js
+++ site/app.js
@@ -6,26 +6,22 @@ app.use (express.urlencoded({extended:false}));
 
 app.listen (3000, ()=> (console.log("Servidor funcionando en puerto 3000")));
 
-
 app.get("/", (req,res) => (res.sendFile(__dirname +"/views/index.html")));
-
 app.get("/cart", (req,res) => (res.sendFile(__dirname +"/views/cart.html")));
-
 app.get("/product-detail", (req,res) => (res.sendFile(__dirname +"/views/product.html")));
-
 app.get("/login", (req,res) => (res.sendFile(__dirname +"/views/login.html")));
-
 app.get("/register", (req,res) => (res.sendFile(__dirname +"/views/register.html")));
-
 app.get("/recover", (req,res) => (res.sendFile(__dirname +"/views/recover.html")));
 
-// Temporal
+// Test
 app.get("/forms", (req,res) => (res.sendFile(__dirname +"/views/partials/forms.html")));
 app.get("/header", (req,res) => (res.sendFile(__dirname +"/views/partials/header.html")));
 app.get("/footer", (req,res) => (res.sendFile(__dirname +"/views/partials/footer.html")));
 app.get("/grid", (req,res) => (res.sendFile(__dirname +"/views/partials/grid.html")));
 
 //Links Temporales
-
 app.post('/login', (req, res) => {res.send(req.url("/login.html"))})
 app.post('/register', (req, res) => {res.send(req.body)});
+
+// Invalid urls 404 errors
+app.get('*', (req, res) => res.sendFile(__dirname + "/views/error.html"));
