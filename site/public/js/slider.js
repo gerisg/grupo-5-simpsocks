2667b993179d6473d610477ce3b4bde59d795ada
Gerardo Gallardo <geris.g@gmail.com>
2020-08-09 03:07:55 -0300

    Product detail refactor. Change photo. Scroll related products.

--- site/public/js/slider.js
+++ site/public/js/slider.js
@@ -4,8 +4,13 @@ function prevSlide() { showSlides(current += -1); }
 function nextSlide() { showSlides(current += 1); }
 function showSlides(n) {
     let slides = document.getElementsByClassName("slide-container");
-    if (n > slides.length) { current = 1 }
-    if (n < 1) { current = slides.length }
+    if (n > slides.length) { 
+        current = 1;
+    } else if (n < 1) { 
+        current = slides.length;
+    } else {
+        current = n;
+    }
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
