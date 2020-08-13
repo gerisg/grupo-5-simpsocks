984577a94a0f421a3c55ae3904cca648a8a2bde6
Gerardo Gallardo <geris.g@gmail.com>
2020-08-13 11:18:21 -0300

    Definition of products controller methods to BREAD

--- site/routes/productRoutes.js
+++ site/routes/productRoutes.js
@@ -2,10 +2,26 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/productController');
 
+// Listado de productos
 router.get('/', controller.list);
-router.get('/detail', controller.detail);
-router.get("/create", controller.create);
-router.get ("/edit", controller.edit);
+
+
+// Formulario de creación de productos
+router.get('/create', controller.create);
+// Acción de creación
+// router.post('/', controller.store);
+
+// Formulario de edición de productos
+router.get('/:id/edit', controller.edit);
+// Acción de edición
+// router.put('/:id', controller.update);
+// Acción de borrado
+// router.delete('/:id', controller.destroy);
+
+// NAV - Carrito de compras
 router.get("/cart", controller.cart);
+
+// Detalle de un producto particular
+router.get('/:id', controller.detail);
 
 module.exports = router;
