e9f85882b7b0f377f55a84f2eeb188364d532c80
Gerardo Gallardo <geris.g@gmail.com>
2020-08-13 10:58:50 -0300

    organize app.js

--- site/app.js
+++ site/app.js
@@ -1,26 +1,24 @@
 const express= require('express');
 const app = express();
 
-// Require routes
+// Routers
 const indexRoutes = require('./routes/indexRoutes');
 const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
 const adminRoutes = require ("./routes/adminRoutes");
 const demoRoutes = require('./routes/demoRoutes');
 
-// Configure
-app.set('view engine', 'ejs')
-app.use(express.static('public'));
+// Configuration
+app.set('view engine', 'ejs'); // views extension ejs
+app.use(express.static('public')); // template engines
 
-// Define routes
+// Routes
 app.use('/', indexRoutes);
 app.use('/users', userRoutes);
 app.use('/products', productRoutes);
 app.use ("/admin", adminRoutes)
 app.use('/demo', demoRoutes);
-
-// Unknown routes
-app.get('*', (req, res) => res.render('error'));
+app.get('*', (req, res) => res.render('error')); // unknown
 
 // Start server
 app.listen (3000, ()=> (console.log('Server listening (3000)')));
