ca3f77e039e50ff729bf1b32a3bae23801d246b1
Vale151 <malbranv@gmail.com>
2020-08-16 00:09:44 -0300

    agrego vista de list y vista de detail en product

--- site/controllers/productController.js
+++ site/controllers/productController.js
@@ -1,7 +1,19 @@
+const path = require('path');
+const fs = require('fs');
+const jsonTable = require('../database/jsonTable');
+
+const productsModel = jsonTable('products');
+
 module.exports = {
-    list: (req, res) => res.render('products/list'),
-    detail: (req,res) => res.render('products/detail'),
-    create: (req,res) => res.render('products/create'),
-    edit: (req,res) => res.render('products/edit'),
-    cart: (req,res) => res.render('products/cart')
+    list: (req, res) => {
+        let products = productsModel.all();
+        res.render('products/list', { products });   
+    },
+    detail: (req,res) =>{
+        let product = productsModel.find(req.params.id);
+        res.render('products/detail', {product});
+    }
+    // create: (req,res) => res.render('products/create'),
+    // edit: (req,res) => res.render('products/edit'),
+    // cart: (req,res) => res.render('products/cart')
 };
--- site/routes/productRoutes.js
+++ site/routes/productRoutes.js
@@ -6,22 +6,22 @@ const controller = require('../controllers/productController');
 router.get('/', controller.list);
 
 
-// Formulario de creación de productos
-router.get('/create', controller.create);
-// Acción de creación
-// router.post('/', controller.store);
+// // Formulario de creación de productos
+// router.get('/create', controller.create);
+// // Acción de creación
+// // router.post('/', controller.store);
 
-// Formulario de edición de productos
-router.get('/:id/edit', controller.edit);
-// Acción de edición
-// router.put('/:id', controller.update);
-// Acción de borrado
-// router.delete('/:id', controller.destroy);
+// // Formulario de edición de productos
+// router.get('/:id/edit', controller.edit);
+// // Acción de edición
+// // router.put('/:id', controller.update);
+// // Acción de borrado
+// // router.delete('/:id', controller.destroy);
 
-// NAV - Carrito de compras
-router.get("/cart", controller.cart);
+// // NAV - Carrito de compras
+// router.get("/cart", controller.cart);
 
-// Detalle de un producto particular
+// // Detalle de un producto particular
 router.get('/:id', controller.detail);
 
 module.exports = router;
