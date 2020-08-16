3910f14eddcfea8898043bd3e649bab15c28fcc3
Vale151 <malbranv@gmail.com>
2020-08-16 00:19:21 -0300

    fix de index con products

--- site/controllers/indexController.js
+++ site/controllers/indexController.js
@@ -1,3 +1,11 @@
+const path = require('path');
+const fs = require('fs');
+const jsonTable = require('../database/jsonTable');
+
+const productsModel = jsonTable('products');
 module.exports = {
-    index: (req,res) => res.render('index')
+    index: (req,res) => {
+        let products = productsModel.all();
+        res.render('index', {products} );
+    }
 }
