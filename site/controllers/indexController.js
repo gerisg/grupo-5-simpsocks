1d1e1790db707cd3709cf887ed6909402d6f4e01
Gerardo Gallardo <geris.g@gmail.com>
2020-08-24 00:51:35 -0300

    Show images from productImagesModel on index view

--- site/controllers/indexController.js
+++ site/controllers/indexController.js
@@ -3,14 +3,25 @@ const fs = require('fs');
 const jsonTable = require('../database/jsonTable');
 
 const productsModel = jsonTable('products');
-const categoriesModel = jsonTable('categories');
+const productImagesModel = jsonTable('productImages');
 
 let priceWithDiscount = (price, discount) => discount > 0 ? Math.round(price * ((100 - discount) / 100)) : price;
+
+let populateProduct = product => {
+    // Add price with discount
+    product.offerPrice = priceWithDiscount(product.price, product.discount);
+    // Add main image
+    let images = productImagesModel.findByField('prodId', product.id);
+    if(images && images.length > 0) { 
+        product.image = images[0].name; 
+    }
+    return product;
+};
 
 module.exports = {
     index: (req,res) => {
         let products = productsModel.all();
-        products.map(p => p.offerPrice = priceWithDiscount(p.price, p.discount));
+        products.map(p => populateProduct(p));
         res.render('index', { products } );
     }
 }
