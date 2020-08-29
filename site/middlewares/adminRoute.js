673f5a0ccf5b494dd8bcfae576ba6e597da361a9
Vale151 <malbranv@gmail.com>
2020-08-29 14:53:20 -0300

    modiique la ruta de administrador

--- site/middlewares/adminRoute.js
+++ site/middlewares/adminRoute.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-    if (req.session.user && req.session.user.category == 'admin') {
+    if (req.session.user && req.session.user.category != 'admin') {
         return res.redirect('/')
     }
     next();
