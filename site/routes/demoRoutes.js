5e14e81933fd3071d42b99fa09ad6ea00616b8e7
Gerardo Gallardo <ggallardo@asanteit.com.ar>
2020-08-30 01:02:09 -0300

    Remove 'demo' section. Remove unused styles.

--- site/routes/demoRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/demoController.js');
-
-router.get('/', controller.index);
-router.get('/forms', controller.forms);
-router.get('/grids', controller.grids);
-
-module.exports = router;
