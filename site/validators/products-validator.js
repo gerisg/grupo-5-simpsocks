624c08389938e8c4ca85cd4ec7a64fd86dbc047c
Gerardo Gallardo <ggallardo@asanteit.com.ar>
2020-08-31 19:58:47 -0300

    Review product validator

--- site/validators/products-validator.js
+++ site/validators/products-validator.js
@@ -6,13 +6,14 @@ module.exports = {
             .notEmpty().withMessage('Debe completar el nombre del producto').bail() //. bail si no se completo no sigue validando
             .isLength({min:5}).withMessage('El nombre debe tener al menos 5 caracteres'),
         check('description')
-            .isLength({min:20}).bail().withMessage('La descripción debe tener al menos 20 caracteres'),
+            .isLength({min:20}).withMessage('La descripción debe tener al menos 20 caracteres').bail(),
         check('price')
-            .notEmpty().withMessage('Debe completar el precio del producto').isNumeric().withMessage('El precio debe ser un número'),
+            .notEmpty().withMessage('Debe completar el precio del producto').bail()
+            .isNumeric().withMessage('El precio debe ser un campo numérico'),
         check('discount')
             .notEmpty().withMessage('Debe completar el descuento del producto').bail()
-            .isNumeric().withMessage('El descuento debe ser un campo numérico (sin el símbolo %)')
-            .isInt({min: 0, max:100}).withMessage('El descuento tiene que ser entre 0 y 100')
+            .isNumeric().withMessage('El descuento debe ser un campo numérico (sin el símbolo %)').bail()
+            .isInt({min: 0, max:100}).withMessage('El descuento debe ser entre 0 y 100')
     ],
     editForm: [
         check('name')
@@ -21,10 +22,11 @@ module.exports = {
         check('description')
             .isLength({min:20}).withMessage('La descripción debe tener al menos 20 caracteres'),
         check('price')
-            .notEmpty().withMessage('Debe completar el precio del producto').isNumeric().withMessage('El precio debe ser un número'),
+            .notEmpty().withMessage('Debe completar el precio del producto').bail()
+            .isNumeric().withMessage('El precio debe ser un campo numérico'),
         check('discount')
             .notEmpty().withMessage('Debe completar el descuento del producto').bail()
-            .isNumeric().withMessage('El descuento debe ser un campo numérico (sin el símbolo %)')
-            .isInt({min: 0, max:100}).withMessage('El descuento tiene que ser entre 0 y 100')
+            .isNumeric().withMessage('El descuento debe ser un campo numérico (sin el símbolo %)').bail()
+            .isInt({min: 0, max:100}).withMessage('El descuento debe ser entre 0 y 100')
     ]
 }
