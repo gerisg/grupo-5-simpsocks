fab8902b82d68d323dcc08b9a823ee4b11b99cbd
Vale151 <malbranv@gmail.com>
2020-08-31 21:22:53 -0300

    fix forms con validaciones

--- site/validators/products-validator.js
+++ site/validators/products-validator.js
@@ -6,21 +6,25 @@ module.exports = {
             .notEmpty().withMessage('Debe completar el nombre del producto').bail() //. bail si no se completo no sigue validando
             .isLength({min:5}).withMessage('El nombre debe tener al menos 5 caracteres'),
         check('description')
-            .isLength({min:20}).withMessage('La descripción debe tener al menos 20 caracteres').bail(),
-            check('price')
-            .notEmpty().withMessage('Debe completar el precio del producto').isNumeric().bail(),
+            .isLength({min:20}).bail().withMessage('La descripción debe tener al menos 20 caracteres'),
+        check('price')
+            .notEmpty().withMessage('Debe completar el precio del producto').isNumeric().withMessage('El precio debe ser un número'),
         check('discount')
-            .notEmpty().withMessage('Debe completar el descuento del producto').isNumeric()
+            .notEmpty().withMessage('Debe completar el descuento del producto').bail()
+            .isNumeric().withMessage('El descuento debe ser un campo numérico (sin el símbolo %)')
+            .isInt({min: 0, max:100}).withMessage('El descuento tiene que ser entre 0 y 100')
     ],
     editForm: [
         check('name')
-        .notEmpty().withMessage('Debe completar el nombre del producto').bail()
-        .isLength({min:5}).withMessage('El nombre debe tener al menos 5 caracteres'),
-    check('description')
-        .isLength({min:20}).withMessage('La descripción debe tener al menos 20 caracteres').bail(),
+            .notEmpty().withMessage('Debe completar el nombre del producto').bail()
+            .isLength({min:5}).withMessage('El nombre debe tener al menos 5 caracteres'),
+        check('description')
+            .isLength({min:20}).withMessage('La descripción debe tener al menos 20 caracteres'),
         check('price')
-        .notEmpty().withMessage('Debe completar el precio del producto').isNumeric().bail(),
-    check('discount')
-        .notEmpty().withMessage('Debe completar el descuento del producto').isNumeric()
+            .notEmpty().withMessage('Debe completar el precio del producto').isNumeric().withMessage('El precio debe ser un número'),
+        check('discount')
+            .notEmpty().withMessage('Debe completar el descuento del producto').bail()
+            .isNumeric().withMessage('El descuento debe ser un campo numérico (sin el símbolo %)')
+            .isInt({min: 0, max:100}).withMessage('El descuento tiene que ser entre 0 y 100')
     ]
 }
