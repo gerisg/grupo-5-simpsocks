676473ba4340fece3d9e5acc0e2c49c99943f891
Gerardo Gallardo <ggallardo@asanteit.com.ar>
2020-09-03 23:37:12 -0300

    Fix middleware admin role conditions.

--- site/middlewares/adminRoute.js
+++ site/middlewares/adminRoute.js
@@ -1,6 +1,6 @@
 /** Agregar este middleware a las rutas que requieran una sesión de usuario de tipo administrador */
 module.exports = (req, res, next) => {
-    if (req.session.user && req.session.user.category != 'admin') {
+    if (!req.session.user || req.session.user.category != 'admin') {
         return res.redirect('/');
     }
     next();
