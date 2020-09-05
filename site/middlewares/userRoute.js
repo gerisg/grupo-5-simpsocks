2bbae7b1e8a01b0bea2ed4fa0e847ddd67057319
Gerardo Gallardo <ggallardo@asanteit.com.ar>
2020-09-05 18:35:08 -0300

    Fix redirect

--- site/middlewares/userRoute.js
+++ site/middlewares/userRoute.js
@@ -1,7 +1,7 @@
 /** Agregar este middleware a las rutas que requieran una sesión de usuario */
 module.exports = (req, res, next) => {
     if (!req.session.user) { 
-        return res.redirect('users/login');
+        return res.redirect('/users/login');
     }
     next();
 }
