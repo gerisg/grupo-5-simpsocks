81e821679da95a09a472c3697c942a1ee98bcb92
Gerardo Gallardo <ggallardo@asanteit.com.ar>
2020-09-14 22:19:47 -0300

    Update product seeders

--- site/database/seeders/20200913234921-product.js
+++ site/database/seeders/20200913234921-product.js
@@ -4,122 +4,122 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('products', [
       {
-        "name": "medias bart cow man",
+        "name": "bart cow man",
         "description": "Medias de algodón. Máxima calidad.",
         "discount": 0
       },
       {
-        "name": "medias bart cow man azules",
+        "name": "bart cow man azules",
         "description": "Medias de algodón. Máxima calidad.",
         "discount": 5
       },
       {
-        "name": "medias bart grises",
+        "name": "bart grises",
         "description": "Medias de algodón. Máxima calidad.",
         "discount": 10
       },
       {
-        "name": "medias bart homme",
+        "name": "bart homme",
         "description": "Medias de algodón. Máxima calidad.",
         "discount": 15
       },
       {
-        "name": "medias homero burger",
+        "name": "homero burger",
         "description": "Medias de algodón. Máxima calidad.",
         "discount": 20
       },
       {
-        "name": "medias homero dona grande",
+        "name": "homero dona grande",
         "description": "Composicion: algodón con lycra, dibujo en poliamida, puño con elastómero. Puño de Lurex.",
         "discount": 25
       },
       {
-        "name": "medias homero comiendo dona",
+        "name": "homero comiendo dona",
         "description": "Composicion: algodón con lycra, dibujo en poliamida, puño con elastómero. Puño de Lurex.",
         "discount": 50
       },
       {
-        "name": "medias homero kit x3",
+        "name": "homero kit x3",
         "description": "Composicion: algodón con lycra, dibujo en poliamida, puño con elastómero. Puño de Lurex.",
         "discount": 0
       },
       {
-        "name": "medias maggie blancas",
+        "name": "maggie blancas",
         "description": "Composicion: algodón con lycra, dibujo en poliamida, puño con elastómero. Puño de Lurex.",
         "discount": 5
       },
       {
-        "name": "medias todos los personajes negras",
+        "name": "todos los personajes negras",
         "description": "Composicion: algodón con lycra, dibujo en poliamida, puño con elastómero. Puño de Lurex.",
         "discount": 0
       },
       {
-        "name": "soquetes bart cow man",
+        "name": "bart cow man",
         "description": "sapien ut nunc vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae mauris viverra diam",
         "discount": 15
       },
       {
-        "name": "medias homero rosquillas blancas",
+        "name": "homero rosquillas blancas",
         "description": "consectetuer adipiscing elit proin interdum mauris non ligula pellentesque ultrices phasellus id sapien in sapien iaculis congue vivamus metus arcu",
         "discount": 20
       },
       {
-        "name": "bucaneras maggie negras",
+        "name": "maggie negras",
         "description": "tristique in tempus sit amet sem fusce consequat nulla nisl nunc nisl duis bibendum",
         "discount": 25
       },
       {
-        "name": "soquetes bart honda",
+        "name": "bart honda",
         "description": "at ipsum ac tellus semper interdum mauris ullamcorper purus sit amet nulla quisque",
         "discount": 50
       },
       {
-        "name": "medias homero rosquillas rosa",
+        "name": "homero rosquillas rosa",
         "description": "nec condimentum neque sapien placerat ante nulla justo aliquam quis turpis",
         "discount": 0
       },
       {
-        "name": "soquetes homero traje banio",
+        "name": "homero traje banio",
         "description": "consectetuer adipiscing elit proin risus praesent lectus vestibulum quam sapien varius ut blandit non interdum in",
         "discount": 5
       },
       {
-        "name": "soquetes homero cielo",
+        "name": "homero cielo",
         "description": "odio in hac habitasse platea dictumst maecenas ut massa quis augue luctus tincidunt nulla mollis molestie lorem",
         "discount": 10
       },
       {
-        "name": "medias homero sparkle",
+        "name": "homero sparkle",
         "description": "nibh in lectus pellentesque at nulla suspendisse potenti cras in purus eu magna vulputate luctus cum sociis",
         "discount": 15
       },
       {
-        "name": "soquetes lisa saxo",
+        "name": "lisa saxo",
         "description": "aliquam quis turpis eget elit sodales scelerisque mauris sit amet eros",
         "discount": 20
       },
       {
-        "name": "soquetes homero desnudo",
+        "name": "homero desnudo",
         "description": "nunc commodo placerat praesent blandit nam nulla integer pede justo lacinia eget tincidunt eget",
         "discount": 25
       },
       {
-        "name": "medias burns",
+        "name": "burns",
         "description": "aliquam quis turpis eget elit sodales scelerisque mauris sit amet eros",
         "discount": 50
       },
       {
-        "name": "medias marge paleta de colores",
+        "name": "marge paleta de colores",
         "description": "at ipsum ac tellus semper interdum mauris ullamcorper purus sit amet nulla quisque",
         "discount": 0
       },
       {
-        "name": "bucaneras lisa gato",
+        "name": "lisa gato",
         "description": "sapien ut nunc vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae mauris viverra diam",
         "discount": 5
       },
       {
-        "name": "bucaneras marge gato",
+        "name": "marge gato",
         "description": "consectetuer adipiscing elit proin interdum mauris non ligula pellentesque ultrices phasellus id sapien in sapien iaculis congue vivamus metus arcu",
         "discount": 10
       }
