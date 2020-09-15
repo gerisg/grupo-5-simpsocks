82f4c3e18e9890d919a31db25f02db9db7e11a48
Gerardo Gallardo <ggallardo@asanteit.com.ar>
2020-09-15 01:52:35 -0300

    Migrated categories to database

--- site/database/models/category.js
+++ site/database/models/category.js
@@ -3,6 +3,7 @@ const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Category extends Model {
     static associate(models) {
+      this.belongsTo(this, { foreignKey: 'parent_id', as: 'parent' });
       this.belongsToMany(models.product, { through:'categories_products' });
     }
   };
