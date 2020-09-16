c9bb3c9fe298d22a1af4df7308b52ca30b43f305
Vale151 <malbranv@gmail.com>
2020-09-16 20:31:57 -0300

    create migrations of  product images

---
+++ site/database/migrations/20200916215218-associate-product-image.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn(
+      'images', // name of Source table
+      'product_id', // name of the key we're adding 
+      {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'products', // name of Target table
+          key: 'id', // key in Target table that we're referencing
+        }
+      }
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn(
+      'images', // name of Source table
+      'product_id' // key we want to remove
+    );
+  }
+};
