77136ca74ca59f2b47429f9d97dc578518b571bc
Vale151 <malbranv@gmail.com>
2020-09-28 16:23:44 -0300

    refactor de validaciones en login

--- site/public/js/login-validations.js
+++ site/public/js/login-validations.js
@@ -5,12 +5,10 @@ window.addEventListener('load', function() {
     let loginForm = document.getElementById('loginForm');
     let email = document.getElementById('email');
     let password = document.getElementById('password');
-    // let recoverForm = document.getElementById('recoverForm');
 
     let validateEmail = function() {
         let feedback = '';
-        // let feedbackElement = email.nextElementSibling;
-
+     
         if(validator.isEmpty(email.value, {ignore_whitespace:true })) {
             feedback = 'El campo no puede estar vacío';
         } else if(!validator.isEmail(email.value)) {
@@ -19,44 +17,18 @@ window.addEventListener('load', function() {
 
         handleFeedback(email, feedback) //recibe el elemento(nombre del input)
 
-        // if(feedback != '') {
-        //     email.classList.add('error');
-        //     feedbackElement.classList.add('error');
-        //     errors.email = feedback;
-        // } else {
-        //     email.classList.remove('error');
-        //     feedbackElement.classList.remove('error');
-        //     delete errors.mail;
-        // }
-
-        // feedbackElement.innerText = feedback; //Le agregamos texto a feedback
-
-        // // email.nextElementSibling.innerText = feedback;
-        // console.log(errors);
-    }
+    };
 
     let validatePassword = function(){
         let feedback = '';
-        // let feedbackElement = password.nextElementSibling;
 
         if(validator.isEmpty(password.value)){
             feedback = 'La contraseña es obligatoria';
         }else if(!validator.isLength(password.value, { min: 8 }))  {
             feedback = 'La contraseña debe tener al menos 8 caracteres';
         }
-        handleFeedback(password, feedback);
-        // if(feedback != ''){
-        //     password.classList.add('error');
-        //     feedbackElement.classList.add('error');
-        //     errors.password = feedback;
-        // }else{
-        //     password.classList.remove('error');
-        //     feedbackElement.classList.remove('error');
-        //     delete errors.password;
-        // }
 
-        // feedbackElement.innerText = feedback;
-        // console.log(errors);
+        handleFeedback(password, feedback);
 
     };
 //Modularizar la funcion para mostrar feedback 
@@ -76,7 +48,7 @@ window.addEventListener('load', function() {
 
         feedbackElement.innerText = feedback;
         console.log(errors);
-    }
+    };
 
     let validateLogin = function(e){
         validateEmail();
@@ -85,7 +57,7 @@ window.addEventListener('load', function() {
         if(object.keys(errors).length) { //objeto con arrays de propiedades
             e.preventDefault();
         }
-    }
+    };
 
     //Agregado de listeners 
 
