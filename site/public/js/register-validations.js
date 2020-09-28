f1243e95605824d7df897ab7d2bb7d2b1e4c70de
Vale151 <malbranv@gmail.com>
2020-09-27 23:15:52 -0300

    add register validations

---
+++ site/public/js/register-validations.js
@@ -0,0 +1,127 @@
+window.addEventListener('load', function(){
+    let errors = {};
+
+    let registerForm = document.getElementById('registerForm');
+    let firstName = document.getElementById('firstname');
+    let lastName = document.getElementById('lastname');
+    let email = document.getElementById('email');
+    let password = document.getElementById('password');
+
+    let validateFisrtName = function() {
+        let feedback = '';
+        let feedbackElement = firstName.nextElementSibling;
+
+        if(validator.isEmpty(firstName.value, {ignore_whitespace:true })){
+            feedback = 'El nombre es obligatorio';
+        } else if (!validator.isLength(firstName.value, { min: 3 })) {
+            feedback = 'El nombre debe tener al menos 3 caracteres';    
+        }
+
+        if(feedback != '') {
+            firstName.classList.add('error');
+            feedbackElement.classList.add('error');
+            errors.firstName = feedback;
+        }else {
+            firstName.classList.remove('error');
+            feedbackElement.classList.remove('error');
+            delete errors.firstName; 
+        }
+
+        feedbackElement.innerHTML = feedback;
+        console.log(errors);
+    }
+
+    let validateLastName = function() {
+        let feedback = '';
+        let feedbackElement = lastName.nextElementSibling;
+
+        if(validator.isEmpty(lastName.value, {ignore_whitespace:true })) {
+            feedback = 'El apellido es obligatorio';
+        } else if(!validator.isLength(firstName.value, { min: 3 }))  {
+            feedback = 'El apellido debe tener al menos 3 caracteres';
+        }
+
+        if(feedback != '') {
+            lastName.classList.add('error');
+            feedbackElement.classList.add('error');
+            errors.lastName = feedback;
+        } else {
+            lastName.classList.remove('error');
+            feedbackElement.classList.remove('error');
+            delete errors.lastName;
+        }
+
+        feedbackElement.innerText = feedback;
+        console.log(errors);
+    }
+
+    let validateEmail = function() {
+        let feedback = '';
+        let feedbackElement = email.nextElementSibling;
+
+        if(validator.isEmpty(email.value, {ignore_whitespace:true })) {
+            feedback = 'El correo electrónico es obligatorio';
+        } else if(!validator.isEmail(email.value)) {
+            feedback = 'El email debe incluir @';
+        }
+
+        if(feedback != '') {
+            email.classList.add('error');
+            feedbackElement.classList.add('error');
+            errors.email = feedback;
+        } else {
+            email.classList.remove('error');
+            feedbackElement.classList.remove('error');
+            delete errors.mail;
+        }
+
+        feedbackElement.innerText = feedback;
+        console.log(errors);
+    }
+
+    let validatePassword = function(){
+        let feedback = '';
+        let feedbackElement = password.nextElementSibling;
+
+        if(validator.isEmpty(password.value)){
+            feedback = 'La contraseña es obligatoria';
+        }else if(!validator.isLength(password.value, { min: 8 }))  {
+            feedback = 'La contraseña debe tener al menos 8 caracteres';
+        }
+
+        if(feedback != ''){
+            password.classList.add('error');
+            feedbackElement.classList.add('error');
+            errors.password = feedback;
+        }else{
+            password.classList.remove('error');
+            feedbackElement.classList.remove('error');
+            delete errors.password;
+        }
+
+        feedbackElement.innerText = feedback;
+        console.log(errors);
+    }
+
+    let validateRegister = function(e) {
+        validateFisrtName();
+        // validatelastName();
+        validateEmail();
+        validatePassword();
+
+        if(object.keys(errors).length) { //objeto con arrays de propiedades
+            e.preventDefault();
+        }
+    }
+
+    
+
+    //Agregando listeners
+
+    firstName.addEventListener('blur', validateFisrtName);
+    lastName.addEventListener('blur', validateLastName);
+    email.addEventListener('blur', validateEmail);
+    password.addEventListener('blur', validatePassword);
+    registerForm.addEventListener('submit', validateRegister);
+
+})
