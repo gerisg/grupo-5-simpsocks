b702cb22c78d9b62014a433d51a56535edac69d8
Vale151 <malbranv@gmail.com>
2020-09-29 11:38:19 -0300

    fix bug in profile

--- site/public/js/profile-validations.js
+++ site/public/js/profile-validations.js
@@ -73,7 +73,7 @@ window.addEventListener('load', function(){
 
     //Modularizar la funcion para mostrar feedback 
     let handleFeedback = function (element, feedback){
-        let feedbackElement = password.nextElementSibling;
+        let feedbackElement = element.nextElementSibling;
 
         if(feedback != ''){
             element.classList.add('error');
@@ -83,7 +83,7 @@ window.addEventListener('load', function(){
         }else{
             element.classList.remove('error');
             feedbackElement.classList.remove('error');
-            delete errors.password;
+            delete errors[element.name];
         }
 
         feedbackElement.innerText = feedback;
