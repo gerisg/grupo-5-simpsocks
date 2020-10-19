2a19bef2fc9844b7b6599b730240578e147af2fb
Gerardo <geris.g@gmail.com>
2020-10-19 00:47:35 -0300

    Few styles

--- dashboard/src/components/cards/SimpleCard.js
+++ dashboard/src/components/cards/SimpleCard.js
@@ -14,7 +14,7 @@ function SimpleCard(props) {
                             <div className="h5 mb-0 font-weight-bold text-gray-800">{props.metric.value}</div>
                         </div>
                         <div className="col-auto">
-                            <i className={` ${props.metric.icon} fa-2x text-gray-300`}></i>
+                            <i className={` ${props.metric.icon} fa-2x text-${props.metric.color}`}></i>
                         </div>
                     </div>
                 </div>
--- dashboard/src/components/layout/topbar/TopbarItemIcon.js
+++ dashboard/src/components/layout/topbar/TopbarItemIcon.js
@@ -3,7 +3,7 @@ import React from 'react';
 function TopbarItemIcon(props) {
     return (
         <li className="nav-item dropdown no-arrow mx-1">
-            <a className="nav-link dropdown-toggle" href="/" id={props.id}>
+            <a className="nav-link dropdown-toggle text-primary" href="/" id={props.id}>
                 <i className={props.icon}></i>
                 <span className="badge badge-danger badge-counter">{props.value}</span>
             </a>
