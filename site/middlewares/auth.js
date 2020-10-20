7e5b7f082c64e2bb4552ac03548b8a2b3e2d241a
Gerardo <geris.g@gmail.com>
2020-10-19 21:03:07 -0300

    Fix database error finding token

--- site/middlewares/auth.js
+++ site/middlewares/auth.js
@@ -6,14 +6,18 @@ module.exports = async (req, res, next) => {
     if (sessionUser) {
         res.locals.auth = sessionUser; // Available in views
     } else if (utCookie) {
-        let userToken = await token.findOne({ where: { token: utCookie }});
-        if (userToken) {
-            let cookieUser = await user.findByPk(userToken.user_id);
-            if (cookieUser) {
-                let userSession = { id: cookieUser.id, name: cookieUser.firstname, category: cookieUser.role_id };
-                req.session.user = userSession; // Available in session
-                res.locals.auth = userSession; // Available in views
+        try {
+            let userToken = await token.findOne({ where: { token: utCookie }});
+            if (userToken) {
+                let cookieUser = await user.findByPk(userToken.user_id);
+                if (cookieUser) {
+                    let userSession = { id: cookieUser.id, name: cookieUser.firstname, category: cookieUser.role_id };
+                    req.session.user = userSession; // Available in session
+                    res.locals.auth = userSession; // Available in views
+                }
             }
+        } catch (error) {
+            // Continue without auth
         }
     }
     next();
