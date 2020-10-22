8bc9342e3455da16a6054e52587c5a33a2cadb1c
Gerardo <geris.g@gmail.com>
2020-10-22 14:32:11 -0300

    Update category images

--- site/database/seeders/20200913235959-category.js
+++ site/database/seeders/20200913235959-category.js
@@ -3,18 +3,21 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('categories', [
-      { name: "destacados", top: 1, image: "originals.png", parent_id: null },
+      { name: "destacados", top: 1, image: "featured.jpg", parent_id: null },
       { name: "personajes", top: 0, image: "", parent_id: null },
       { name: "promociones", top: 0, image: "", parent_id: null },
-      { name: "homero", top: 1, image: "homero.png", parent_id: 2 },
+      { name: "homero", top: 0, image: "", parent_id: 2 },
       { name: "marge", top: 0, image: "", parent_id: 2 },
-      { name: "bart", top: 1, image: "sockets.jpg", parent_id: 2 },
+      { name: "bart", top: 1, image: "bart.jpg", parent_id: 2 },
       { name: "lisa", top: 0, image: "", parent_id: 2 },
       { name: "maggie", top: 0, image: "", parent_id: 2 },
-      { name: "hotsale", top: 1, image: "bucaneras.png", parent_id: 3 },
+      { name: "hotsale", top: 0, image: "", parent_id: 3 },
       { name: "descuentos", top: 0, image: "", parent_id: 3 },
       { name: "envios", top: 0, image: "", parent_id: 3 },
-      { name: "2x1", top: 0, image: "", parent_id: 3 }
+      { name: "2x1", top: 0, image: "", parent_id: 3 },
+      { name: "tamaño", top: 0, image: "", parent_id: null },
+      { name: "adultos", top: 1, image: "adult.jpg", parent_id: 13 },
+      { name: "niños", top: 1, image: "children.jpg", parent_id: 13 },
     ], {});
   },
 
