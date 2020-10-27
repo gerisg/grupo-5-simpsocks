df49329b33847faf8fb4ee96032d168bef6da3ae
Gerardo <geris.g@gmail.com>
2020-10-27 12:38:12 -0300

    Calculate cart resume

--- site/public/js/cart.js
+++ site/public/js/cart.js
@@ -1,4 +1,5 @@
 let itemTemplate = document.getElementById('item-template');
+let resumeCart = document.getElementById('resume');
 
 function getItems() {
     let cart = localStorage.getItem('simpsocks_cart');
@@ -52,70 +53,104 @@ function decrement(id, count=1) {
     }
     console.error('Not found item: ' + id);
 }
-
+/* Devuelve el precio total con el descuento aplicado */
 function calculateTotalPrice(price, discount, count) {
     return count * (discount > 0 ? Math.round(price * ((100 - discount) / 100)) : price);
 }
 
-let items = getItems();
-
-for (let i = 0; i < items.length; i++) {
-    const item = items[i];
-    let newItemNode = itemTemplate.cloneNode(true);
-
-    // Static data
-    newItemNode.setAttribute('id', item.id);
-    newItemNode.childNodes[1].src = `/images/products/${item.image}`;
-    newItemNode.childNodes[3].childNodes[3].innerHTML = `${item.name}<br /><span class="text-small">${item.detail}</span>`;
-    
-    // Calculate total
-    const price = item.price;
-    const discount = item.discount;
-    const totalPrice = calculateTotalPrice(price, discount, item.count);
-    
-    // Item discount
-    let discountElement = newItemNode.childNodes[3].childNodes[7].childNodes[1].childNodes[1];
-    if(discount > 0) {
-        discountElement.innerText = discount + '%';
+function fetchResumeCart() {
+    let items = getItems();
+    if(!items.length) {
+        resumeCart.parentElement.innerHTML = '<div class="text">Carrito vacio</div>';
+        return;
     }
-    // Item offer price
-    let originalPriceElement = newItemNode.childNodes[3].childNodes[7].childNodes[1].childNodes[3];
-    if(discount > 0) {
-        originalPriceElement.innerText = `$ ${price * item.count}`;
+    // Initialize
+    let total = 0;
+    let totalDiscount = 0;
+    resumeCart.children[0].innerHTML = '';
+    resumeCart.children[1].innerHTML = '';
+    // Calculate
+    for (let i = 0; i < items.length; i++) {
+        const item = items[i];
+        const totalPrice = item.price * item.count;
+        total += totalPrice;
+        if(item.discount) {
+            totalDiscount += (totalPrice - calculateTotalPrice(item.price, item.discount, item.count));
+        }
+        resumeCart.children[0].innerHTML += `<div class="resume-line"><span>${item.name}:</span><span>$${totalPrice}</span></div>`;
     }
-    // Item total price
-    let priceElement = newItemNode.childNodes[3].childNodes[7].childNodes[1].childNodes[5];
-    priceElement.innerText = `$ ${totalPrice}`;
-
-    // Item count
-    let countElement = newItemNode.childNodes[3].childNodes[5].childNodes[3].childNodes[2];
-    countElement.value = item.count;
-
-    // Decrement count listener
-    newItemNode.childNodes[3].childNodes[5].childNodes[3].childNodes[1].addEventListener('click', e => {
-        const updatedCount = decrement(item.id);
-        countElement.value = updatedCount;
-        priceElement.innerText = `$ ${calculateTotalPrice(price, discount, updatedCount)}`;
-        if(discount > 0) {
-            originalPriceElement.innerText = `$ ${price * updatedCount}`;
-        }
-    });
-
-    // Increment count listener
-    newItemNode.childNodes[3].childNodes[5].childNodes[3].childNodes[3].addEventListener('click', e => {
-        const updatedCount = increment(item.id);
-        countElement.value = updatedCount;
-        priceElement.innerText = `$ ${calculateTotalPrice(price, discount, updatedCount)}`;
-        if(discount > 0) {
-            originalPriceElement.innerText = `$ ${price * updatedCount}`;
-        }
-    });
-
-    // Remove item
-    newItemNode.childNodes[3].childNodes[1].addEventListener('click', e => {
-        removeItem(item.id);
-        newItemNode.parentNode.removeChild(newItemNode);
-    });
-    
-    itemTemplate.nextElementSibling.insertBefore(newItemNode, null);
+    resumeCart.children[1].innerHTML += `<div class="resume-line"><span>Descuentos:</span><span>-$${totalDiscount}</span></div>`;
+    resumeCart.children[3].innerHTML = `<div class="resume-total"><span>Total a Pagar:</span><span>$${total - totalDiscount}</span></div>`;
 }
+
+function init() {
+    
+    // Load resume cart
+    fetchResumeCart();
+
+    // Load items cart
+    let items = getItems();
+    for (let i = 0; i < items.length; i++) {
+        const item = items[i];
+        let newItemNode = itemTemplate.cloneNode(true);
+
+        // Static data
+        newItemNode.setAttribute('id', item.id);
+        newItemNode.childNodes[1].src = `/images/products/${item.image}`;
+        newItemNode.childNodes[3].childNodes[3].innerHTML = `${item.name}<br /><span class="text-small">${item.detail}</span>`;
+        
+        // Calculate total
+        const totalPrice = calculateTotalPrice(item.price, item.discount, item.count);
+        
+        // Item discount
+        let discountElement = newItemNode.childNodes[3].childNodes[7].childNodes[1].childNodes[1];
+        if(item.discount > 0) {
+            discountElement.innerText = item.discount + '%';
+        }
+        // Item offer price
+        let originalPriceElement = newItemNode.childNodes[3].childNodes[7].childNodes[1].childNodes[3];
+        if(item.discount > 0) {
+            originalPriceElement.innerText = `$ ${item.price * item.count}`;
+        }
+        // Item total price
+        let priceElement = newItemNode.childNodes[3].childNodes[7].childNodes[1].childNodes[5];
+        priceElement.innerText = `$ ${totalPrice}`;
+
+        // Item count
+        let countElement = newItemNode.childNodes[3].childNodes[5].childNodes[3].childNodes[2];
+        countElement.value = item.count;
+
+        // Decrement count listener
+        newItemNode.childNodes[3].childNodes[5].childNodes[3].childNodes[1].addEventListener('click', e => {
+            const updatedCount = decrement(item.id);
+            countElement.value = updatedCount;
+            priceElement.innerText = `$ ${calculateTotalPrice(item.price, item.discount, updatedCount)}`;
+            if(item.discount > 0) {
+                originalPriceElement.innerText = `$ ${item.price * updatedCount}`;
+            }
+            fetchResumeCart();
+        });
+
+        // Increment count listener
+        newItemNode.childNodes[3].childNodes[5].childNodes[3].childNodes[3].addEventListener('click', e => {
+            const updatedCount = increment(item.id);
+            countElement.value = updatedCount;
+            priceElement.innerText = `$ ${calculateTotalPrice(item.price, item.discount, updatedCount)}`;
+            if(item.discount > 0) {
+                originalPriceElement.innerText = `$ ${item.price * updatedCount}`;
+            }
+            fetchResumeCart();
+        });
+
+        // Remove item
+        newItemNode.childNodes[3].childNodes[1].addEventListener('click', e => {
+            removeItem(item.id);
+            newItemNode.parentNode.removeChild(newItemNode);
+            fetchResumeCart();
+        });
+        
+        itemTemplate.nextElementSibling.insertBefore(newItemNode, null);
+    }
+}
+
+init();
